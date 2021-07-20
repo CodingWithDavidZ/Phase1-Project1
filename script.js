c5920f244dd62ce11691e415573aee8898f12088
David Zombar <davidzombar@gmail.com>
2021-07-19 21:05:48 -0600

    working info list displays

--- script.js
+++ script.js
@@ -14,14 +14,37 @@ form.addEventListener("submit", function (e) {
     .then((result) => result.json())
     .then((data) => {
       console.log(data);
-      const hireable = `Hireable: ${data.hirable}`;
+
+      //pulled data to use in lists
+      const hireable = `Hireable: ${data.hireable}`;
+      const location = `Location: ${data.location}`;
+      const fullName = `Name: ${data.name}`;
+      const email = `Email: ${data.email}`;
+      const repoCount = `Repo Count: ${data.public_repos}`;
+      const created = `Created: ${data.created_at}`;
+      const lastUpdated = `Last update: ${data.updated_at}`;
+      const twitterHandle = `Twitter: ${data.twitter_username}`;
 
       //display picture that when click, redirects to thier github
       document.getElementById("searchResult").innerHTML = `
         <a target="_blank" href="https://www.github.com/${originalName}"> <img src= "${data.avatar_url}"/></a>
       `;
 
-      document.getElementById("moreInfo1").innerHTML = hireable;
+      const basicInfoArray = [fullName, location, email, twitterHandle];
+      var str = "<ul>";
+
+      basicInfoArray.forEach(function (slide) {
+        str += "<li>" + slide + "</li>";
+      });
+      str += "</ul>";
+      document.getElementById("moreInfo1").innerHTML = str;
+
+      const moreInfoArray = [created, lastUpdated, repoCount, hireable];
+      var str2 = "<ul>";
+      moreInfoArray.forEach(function (slide) {
+        str2 += "<li>" + slide + "</li>";
+      });
+      str2 += "</ul>";
+      document.getElementById("moreInfo2").innerHTML = str2;
     });
-  // document.getElementById("moreInfo2").innerHTML =
 });
